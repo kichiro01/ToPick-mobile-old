d76c8284b93b78337af6d9c4b538bc8061c0d0d5
kichiro01 <kichiro01@gmail.com>
2022-07-28 23:08:40 +0900

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const baseColor = Color.fromRGBO(249, 212, 35, 1);
+  static const subColor = Color.fromRGBO(53, 92, 125, 1);
+}
