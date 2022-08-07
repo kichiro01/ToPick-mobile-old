75b93af008e8a53d359f2246a8900f5e5ceda184
kichiro01 <kichiro01@gmail.com>
2022-08-08 02:13:12 +0900

    カテゴリーページ実装

--- lib/pages/SelectPage.dart
+++ lib/pages/SelectPage.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:topick/codes/SelectionTypeCode.dart';
@@ -18,63 +19,80 @@ class SelectPage extends StatefulWidget {
 }
 
 class _SelectPageState extends State<SelectPage> {
-  // int _counter = 0;
-  void _incrementCounter() {
+  List<Map<String, Object>>? _selectionInfo;
+  int _spotNum = 0;
+  Size? _screenSize;
+  double? _cardHeight;
+  double _largeFlickVelocityThreshold = 1600.0;
+  double _minimumFlickVelocityThreshold = 1000.0;
+
+  void _setSpotNum(int num) {
     setState(() {
+      _spotNum = num;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    _screenSize = MediaQuery.of(context).size;
+    _cardHeight = _screenSize!.height * 55 / 111;
+
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.selectionType.getLabel(), style: const TextStyle(color: AppConstants.subColor, fontWeight: FontWeight.bold),),
           backgroundColor: AppConstants.baseColor,
           iconTheme: const IconThemeData(color: AppConstants.subColor),
         ),
-        body: _body(context)
+        body: _body()
     );
   }
 
-  Widget _body(BuildContext context) {
-    final selectionInfo = widget.selectionType == SelectionType.category ? Categories.categories: Scenes.scenes;
-    final name = widget.selectionType == SelectionType.category ? Categories.categories: Scenes.scenes;
+  Widget _body() {
+    _selectionInfo = widget.selectionType == SelectionType.category ? Categories.categories: Scenes.scenes;
 
     return Container(
         margin: const EdgeInsets.only(top: 46),
         child: Center(
           child: Column(
             children:  [
-              Container(margin: const EdgeInsets.only(bottom: 11), child: const Text("一般", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,),),),
-              Container(margin: const EdgeInsets.only(bottom: 2), child: const Text("一般的な話題を提供します。", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: AppConstants.darkGrey))),
-              Container(margin: const EdgeInsets.only(bottom: 50), child: const Text("困った時はこれ！", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: AppConstants.darkGrey))),
-              _selectionArea(context, selectionInfo)
+              Container(margin: const EdgeInsets.only(bottom: 11), child: Text((_selectionInfo![_spotNum]['category_name'] as List<String>)[0], style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold,),),),
+              Container(margin: const EdgeInsets.only(bottom: 2), child: Text((_selectionInfo![_spotNum]['description1'] as List<String>)[0], style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: AppConstants.darkGrey))),
+              Container(margin: const EdgeInsets.only(bottom: 50), child: Text((_selectionInfo![_spotNum]['description2'] as List<String>)[0], style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: AppConstants.darkGrey))),
+              _selectionArea()
             ],
           ),
         )
     );
   }
 
-  Widget _selectionArea(BuildContext context, List<Map<String, Object>> selectionInfo) {
-    final Size screenSize = MediaQuery.of(context).size;
-    return Container(
-      height: screenSize.height * 55 / 111,
-      child: ListView.builder(
+  Widget _selectionArea() {
+    return CarouselSlider(
+        items:  _selectionInfo!.map((value) {
+          return Builder(
+            builder: (BuildContext context) {
+              return _selectionCards(value);
+            },
+          );
+        }).toList(),
+        options: CarouselOptions(
+          height: _cardHeight!,
+          initialPage: 0,
+          autoPlay: false,
+          onPageChanged:(index, reason) => _setSpotNum(index),
+          viewportFraction: 0.6,
+          enableInfiniteScroll: false,
+          reverse: false,
+          enlargeCenterPage: true,
           scrollDirection: Axis.horizontal,
-          itemCount: selectionInfo.length,
-          itemBuilder: (context, index) {
-            final snippet = selectionInfo[index]['img_url_snippet'] as String;
-            final double marginLeft = index == 0 ? (screenSize.height / 12.8) : 0;
-            final double marginRight = index == selectionInfo.length - 1 ? (screenSize.height / 12.8) : 0;
-            return _selectionCards(snippet, marginLeft, marginRight);
-          }),
-      );
+        ),
+    );
   }
-  
-  Widget _selectionCards(String snippet, double marginLeft, double marginRight) {
+
+  Widget _selectionCards(Map<String, Object> selection) {
+    final snippet = selection['img_url_snippet'] as String;
     var imgUrl = 'assets/images/$snippet/$snippet@2x.png';
-    return Container(
-      margin: EdgeInsets.only(left: marginLeft, right: marginRight),
+    return GestureDetector(
+      onTap: () => print(selection['category_id']),
       child: Image.asset(imgUrl, fit: BoxFit.fitHeight,),
     );
   }
